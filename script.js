b05b1c7e09a761a3b238915dbe7b4a2d9be8d760
핑구 <58054397+sihoo67@users.noreply.github.com>
2025-02-26 22:50:45 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+    // '로그인 완료' 텍스트에 애니메이션 효과 추가
+    document.querySelector('h1').classList.add('neon');
+    document.querySelector('.message').classList.add('flicker');
+}
